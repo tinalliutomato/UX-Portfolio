f218f44875274510e3edb0e77d2ad1169df0ce13
tinalliutomato <xihongtina.liu@gmail.com>
2017-03-17 18:12:10 -0400

    background, fix highlight section

--- js/section-highlight.js
+++ js/section-highlight.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
 
-    var sectionReady = $('.latched').length;
-    if (1) {
-        $sections = $('section');
-        
+    $sections = $('section');
 
-        /* Store the original positions */
-
-        /* respond to the scroll event */
-        $(document).scroll(function(){
+    $(document).scroll(function(){
         /* get the current scroll position */
         var currentScroll  = $(document).scrollTop();
         /* change classes based on section positions */
@@ -17,29 +11,27 @@ $(document).ready(function() {
 
         // We check the position of each of the divs compared to the windows scroll positon
         $sections.each(function(){
-        // divPosition is the position down the page in px of the current section we are testing      
-        var divPosition = $(this).offset().top;
+            // divPosition is the position down the page in px of the current section we are testing      
+            var divPosition = $(this).offset().top;
 
-        // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
-        // the -1 is so that it includes the div 1px before the div leave the top of the window.
-        if( divPosition - 1 < currentScroll ){
-            // We have either read the section or are currently reading the section so we'll call it our current section
-            $currentSection = $(this);
+            // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+            // the -1 is so that it includes the div 1px before the div leave the top of the window.
+            if( divPosition - 100 < currentScroll ){
+                // We have either read the section or are currently reading the section so we'll call it our current section
+                $currentSection = $(this);
 
-            // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
-        }
+                // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+            }
 
-        // This is the bit of code that uses the currentSection as its source of ID
-        if ($currentSection) {
-            $sections.removeClass('active');
-            $currentSection.addClass('active');
-        }
+            // This is the bit of code that uses the currentSection as its source of ID
+            if ($currentSection) {
+                $sections.removeClass('active');
+                $currentSection.addClass('active');
+            }
 
 
         })
 
-        });
-    }
-
+    });
  
 });
