27601e7ce59b43ea0594db41acb518dcd9c7255a
tinalliutomato <xihongtina.liu@gmail.com>
2017-04-03 17:15:54 -0400

    remove active of first one when scrolled to less than half

--- js/section-highlight.js
+++ js/section-highlight.js
@@ -7,28 +7,35 @@ $(document).ready(function() {
     $(document).scroll(function(){
             /* get the current scroll position */
             var currentScroll  = $(document).scrollTop();
-            //debugger;
+
+            var windowHeight = window.innerHeight;
+            var half = windowHeight / 2;
+            var fifth = windowHeight / 5;
             $sections.each(function(){
                     // divPosition is the position down the page in px of the current section we are testing      
                     var divPosition = $(this).offset().top;
 
                     if (currentScroll > lastScroll) {
-                        if( divPosition - (window.innerHeight/2) < currentScroll ){
+                        if (divPosition - half < currentScroll ){
                             // We have either read the section or are currently reading the section so we'll call it our current section
                             $currentSection = $(this);
                             // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
                         }
                     } else {
-                        if( divPosition - (window.innerHeight/2) < currentScroll ){
+                        if (divPosition - half < currentScroll ){
                             // We have either read the section or are currently reading the section so we'll call it our current section
                             $currentSection = $(this);
                             // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
                         }
+                        
                     }
 
                     if ($currentSection) {
                         $sections.removeClass('active');
                         $currentSection.addClass('active');
+                        if (currentScroll < half) {
+                            $currentSection.removeClass('active');
+                        }
                     }
             })
 
